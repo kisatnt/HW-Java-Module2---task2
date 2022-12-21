fbef20261ea6ca56c34e384a3904f7e2eef70007
Andrei Kiselev <eparis@atlassian.com>
2022-12-21 14:20:45 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceAcc = 100; //задаем первоначальный баланс
+        int topUp = 1020; //задаем сумму пополнения
+        int cashBack = topUp / 100; // считаем бонус
+        int amountFinal; // обьявляем итоговую переменную баланса
+        if (topUp < 101) { // проверяем условие зачисления бонусов
+            amountFinal = balanceAcc + topUp; //считаем сумму без бонуса
+        } else {
+            amountFinal = balanceAcc + topUp + cashBack; //считаем сумму с бонусом
+        }
+        System.out.println(amountFinal);
+
+    }
+}

341dcc53523e28958a3f1a302134e1c788120bb5
Andrei Kiselev <eparis@atlassian.com>
2022-12-26 19:14:22 +0700

    add rules topup < 1000

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int balanceAcc = 100; //задаем первоначальный баланс
-        int topUp = 1020; //задаем сумму пополнения
+        int topUp = 999; //задаем сумму пополнения
         int cashBack = topUp / 100; // считаем бонус
         int amountFinal; // обьявляем итоговую переменную баланса
         if (topUp < 1001) { // проверяем условие зачисления бонусов
             amountFinal = balanceAcc + topUp; //считаем сумму без бонуса
+            cashBack = 0;
         } else {
             amountFinal = balanceAcc + topUp + cashBack; //считаем сумму с бонусом
         }
